d48e7a130c898752275156fa38d320a740b97674
Alex Bates <ajbates93@gmail.com>
2023-12-09 16:52:09 +0000

    feat: progress on day 2

--- 2023/2/main.go
+++ 2023/2/main.go
@@ -15,17 +15,20 @@ var REQUESTED_COLOURS = map[string]int{"red": 12, "green": 13, "blue": 14}
 func isPossible(gameTotals, colours map[string]int) bool {
 	for colour, amount := range colours {
 		if gameTotals[colour] < amount {
-			return false
+			return true
+		} else {
+			break
 		}
 	}
-	return true
+	return false
 }
 
 // Calculations --------
 
 func parseGame(game string) map[string]int {
 	totals := make(map[string]int)
-	for _, round := range strings.Split(game, ";") {
+	formatted := strings.Split(game, ": ")
+	for _, round := range strings.Split(formatted[1], "; ") {
 		for _, pack := range strings.Split(round, ", ") {
 			amount, colour := parsePack(pack)
 			totals[colour] += amount
@@ -44,6 +47,13 @@ func parsePack(pack string) (int, string) {
 func main() {
 	fileName := os.Args[1]
 	lines, err := utils.ReadFileByLine(fileName)
+	// var staticLines = []string{
+	// 	"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+	// 	"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+	// 	"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+	// 	"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+	// 	"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	// }
 
 	var possibleGames []int
 	sum := 0
